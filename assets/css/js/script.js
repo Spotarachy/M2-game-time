5a382f1f4cfbb00460e3f0a7bf00d80f766576f7
Spotarachy <wilson.f.mario@gmail.com>
2021-07-13 20:40:35 +0000

    added info for the game

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,1 +1,27 @@
 function game (){}  
+let countEl = documrnt.getElementById("count-el")
+
+console.log(couintEl)
+
+let count = 0
+
+function increment () {
+    count = count + 1
+    countEl.innerTrext =count
+}
+
+/* this is a exp loop 
+
+let i = 1;
+while (i <= 10 ) {
+    console.log (i);
+    i++;
+}
+
+*/
+
+// save a value to  localSorge
+// Delete your code and refresh the page 
+// fetch your value from localStorage and log it out 
+
+localStorage.getItem("myCredits")
